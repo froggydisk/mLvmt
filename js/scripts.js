caa346c6ac462b12cfdf4abd8b3b3dcefd0f96f6
froggydisk <61578997+froggydisk@users.noreply.github.com>
2021-12-03 16:51:58 +0900

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,443 @@
+let mydata = {
+  // 0.8.8 version
+  "release 0.8.8":  
+  {
+    "release_version": '0.8.8',
+    "release_date": '2021-12-03',
+    "sprint": 'sprint 13',
+    "summary":{
+      "1": 'finish hall design',
+      "2": 'fix issue #154 ',
+      "3": 'SNS service feature',
+    },
+    "description":{
+      "added":{
+        "1": '1. follow/following feature'
+      },
+      "revised":{
+        "1": '1. clapping gesture/sound',
+      },
+      "deprecated":{
+        "1": '1. You cannot play the music while talking',
+      },
+      "bugfix":{
+        "1": '1. big bug',
+      },
+    },
+    "ios": {
+      "version": '0.8.3',
+      "startdate": '2021-11-20',
+      "enddate": '2021-12-03',
+    },
+    "mac": {
+      "version": '0.7.0',
+      "startdate": '2021-12-01',
+      "enddate": '2021-12-03',
+    },
+    "server": {
+      "version": '0.6.0',
+      "startdate": '2021-11-25',
+      "enddate": '2021-12-03',
+    },
+  },
+
+  // 0.8.7 version
+  "release 0.8.7":  
+  {
+    "release_version": '0.8.7',
+    "release_date": '2021-11-20',
+    "sprint": 'sprint 12',
+    "summary":{
+      "1": 'SNS service feature',
+      "2": 'finish hall design',
+      "3": 'fix issue #154 ',
+    },
+    "description":{
+      "added":{
+        "1": '1. follow/following feature!'
+      },
+      "revised":{
+        "1": '1. clapping gesture/sound!',
+      },
+      "deprecated":{
+        "1": '1. You cannot play the music while talking!',
+      },
+      "bugfix":{
+        "1": '1. big bug!',
+      },
+    },
+    "ios": {
+      "version": '0.8.2',
+      "startdate": '2021-11-08',
+      "enddate": '2021-11-20',
+    },
+    "mac": {
+      "version": '0.6.9',
+      "startdate": '2021-11-20',
+      "enddate": '2021-12-01',
+    },
+    "server": {
+      "version": '0.5.9',
+      "startdate": '2021-11-10',
+      "enddate": '2021-11-25',
+    },
+  },
+
+  // 0.8.6 version
+  "release 0.8.6":  
+  {
+    "release_version": '0.8.6',
+    "release_date": '2021-11-04',
+    "sprint": 'sprint 11',
+    "summary":{
+      "1": 'fix issue #154 ',
+      "2": 'finish hall design',
+      "3": 'SNS service feature',
+    },
+    "description":{
+      "added":{
+        "1": '1. follow/following feature@'
+      },
+      "revised":{
+        "1": '1. clapping gesture/sound@',
+      },
+      "deprecated":{
+        "1": '1. You cannot play the music while talking@',
+      },
+      "bugfix":{
+        "1": '1. big bug@',
+      },
+    },
+    "ios": {
+      "version": '0.8.1',
+      "startdate": '2021-10-29',
+      "enddate": '2021-11-08',
+    },
+    "mac": {
+      "version": '0.6.8',
+      "startdate": '2021-11-14',
+      "enddate": '2021-11-20',
+    },
+    "server": {
+      "version": '0.5.8',
+      "startdate": '2021-11-03',
+      "enddate": '2021-11-10',
+    },
+  },
+
+  // 0.8.5 version
+  "release 0.8.5":  
+  {
+    "release_version": '0.8.5',
+    "release_date": '2021-10-20',
+    "sprint": 'sprint 10',
+    "summary":{
+      "1": 'finish hall design',
+      "2": 'fix issue #154 ',
+      "3": 'SNS service feature',
+    },
+    "description":{
+      "added":{
+        "1": '1. follow/following feature#'
+      },
+      "revised":{
+        "1": '1. clapping gesture/sound#',
+      },
+      "deprecated":{
+        "1": '1. You cannot play the music while talking#',
+      },
+      "bugfix":{
+        "1": '1. big bug#',
+      },
+    },
+    "ios": {
+      "version": '0.8.0',
+      "startdate": '2021-10-03',
+      "enddate": '2021-10-07',
+    },
+    "mac": {
+      "version": '0.6.7',
+      "startdate": '2021-10-05',
+      "enddate": '2021-10-14',
+    },
+    "server": {
+      "version": '0.5.7',
+      "startdate": '2021-10-13',
+      "enddate": '2021-10-29',
+    },
+  },
+
+  // 0.8.4 version
+  "release 0.8.4":  
+  {
+    "release_version": '0.8.4',
+    "release_date": '2021-10-06',
+    "sprint": 'sprint 9',
+    "summary":{
+      "1": 'SNS service feature',
+      "2": 'finish hall design',
+      "3": 'fix issue #154 ',
+    },
+    "description":{
+      "added":{
+        "1": '1. follow/following feature$'
+      },
+      "revised":{
+        "1": '1. clapping gesture/sound$',
+      },
+      "deprecated":{
+        "1": '1. You cannot play the music while talking$',
+      },
+      "bugfix":{
+        "1": '1. big bug$',
+      },
+    },
+    "ios": {
+      "version": '0.7.9',
+      "startdate": '2021-09-03',
+      "enddate": '2021-09-07',
+    },
+    "mac": {
+      "version": '0.6.6',
+      "startdate": '2021-09-05',
+      "enddate": '2021-09-14',
+    },
+    "server": {
+      "version": '0.5.6',
+      "startdate": '2021-09-13',
+      "enddate": '2021-09-29',
+    },
+  },
+
+  // 0.8.3 version
+  "release 0.8.3":  
+  {
+    "release_version": '0.8.3',
+    "release_date": '2021-09-23',
+    "sprint": 'sprint 8',
+    "summary":{
+      "1": 'fix issue #154 ',
+      "2": 'finish hall design',
+      "3": 'SNS service feature',
+    },
+    "description":{
+      "added":{
+        "1": '1. follow/following feature%'
+      },
+      "revised":{
+        "1": '1. clapping gesture/sound%',
+      },
+      "deprecated":{
+        "1": '1. You cannot play the music while talking%',
+      },
+      "bugfix":{
+        "1": '1. big bug%',
+      },
+    },
+    "ios": {
+      "version": '0.6.9',
+      "startdate": '2021-08-03',
+      "enddate": '2021-08-07',
+    },
+    "mac": {
+      "version": '0.6.5',
+      "startdate": '2021-08-05',
+      "enddate": '2021-08-14',
+    },
+    "server": {
+      "version": '0.5.5',
+      "startdate": '2021-08-13',
+      "enddate": '2021-08-29',
+    },
+  },
+
+  // 0.8.2 version
+  "release 0.8.2":  
+  {
+    "release_version": '0.8.2',
+    "release_date": '2021-09-10',
+    "sprint": 'sprint 7',
+    "summary":{
+      "1": 'finish hall design',
+      "2": 'fix issue #154 ',
+      "3": 'SNS service feature',
+    },
+    "description":{
+      "added":{
+        "1": '1. follow/following feature^'
+      },
+      "revised":{
+        "1": '1. clapping gesture/sound^',
+      },
+      "deprecated":{
+        "1": '1. You cannot play the music while talking^',
+      },
+      "bugfix":{
+        "1": '1. big bug^',
+      },
+    },
+    "ios": {
+      "version": '0.6.8',
+      "startdate": '2021-07-03',
+      "enddate": '2021-07-07',
+    },
+    "mac": {
+      "version": '0.6.4',
+      "startdate": '2021-07-05',
+      "enddate": '2021-07-14',
+    },
+    "server": {
+      "version": '0.5.4',
+      "startdate": '2021-07-13',
+      "enddate": '2021-07-29',
+    },
+  },
+}
+
+function reply_click(clicked_id)
+{
+  let release_version = clicked_id
+  var rv = document.getElementsByClassName('release_version');
+  var rd = document.getElementsByClassName('release_date');
+  var sp = document.getElementsByClassName('sprint');
+  var i_v = document.getElementsByClassName('ios_version');
+  var i_s = document.getElementsByClassName('ios_startdate');
+  var i_e = document.getElementsByClassName('ios_enddate');
+  var m_v = document.getElementsByClassName('mac_version');
+  var m_s = document.getElementsByClassName('mac_startdate');
+  var m_e = document.getElementsByClassName('mac_enddate');
+  var s_v = document.getElementsByClassName('server_version');
+  var s_s = document.getElementsByClassName('server_startdate');
+  var s_e = document.getElementsByClassName('server_enddate');
+  var s_1 = document.getElementsByClassName('summary_1');
+  var s_2 = document.getElementsByClassName('summary_2');
+  var s_3 = document.getElementsByClassName('summary_3');
+  var d_a_1 = document.getElementsByClassName('description_added_1');
+  var d_r_1 = document.getElementsByClassName('description_revised_1');
+  var d_d_1 = document.getElementsByClassName('description_deprecated_1');
+  var d_b_1 = document.getElementsByClassName('description_bugfix_1');
+
+  var display = function(m, k){
+    for (let i = 0; i < m.length; i++) {
+        const e = m[i];
+        e.innerText = mydata[release_version][k];
+    }   
+  };
+  var display2 = function(m, k1, k2){
+    for (let i = 0; i < m.length; i++) {
+        const e = m[i];
+        e.innerText = mydata[release_version][k1][k2];
+    }   
+  };
+  var display3 = function(m, k1, k2, k3){
+    for (let i = 0; i < m.length; i++) {
+        const e = m[i];
+        e.innerText = mydata[release_version][k1][k2][k3];
+    }   
+  };
+
+  display(rv, 'release_version')
+  display(rd, 'release_date')
+  display(sp, 'sprint')
+  display2(i_v, 'ios', 'version')
+  display2(i_s, 'ios', 'startdate')
+  display2(i_e, 'ios', 'enddate')
+  display2(m_v, 'mac', 'version')
+  display2(m_s, 'mac', 'startdate')
+  display2(m_e, 'mac', 'enddate')
+  display2(s_v, 'server', 'version')
+  display2(s_s, 'server', 'startdate')
+  display2(s_e, 'server', 'enddate')
+  display2(s_1, 'summary', '1')
+  display2(s_2, 'summary', '2')
+  display2(s_3, 'summary', '3')
+  display3(d_a_1, 'description', 'added', '1')
+  display3(d_r_1, 'description', 'revised', '1')
+  display3(d_d_1, 'description', 'deprecated', '1')
+  display3(d_b_1, 'description', 'bugfix', '1')
+
+
+  var options = {
+    chart: {
+      height: "100%",
+      type: "rangeBar",
+      toolbar: {
+        show: false,
+      },
+    },
+    plotOptions: {
+      bar: {
+        horizontal: true,
+        // dataLabels: {
+        //   position: 'top',
+        // }
+      },
+    },
+    grid: {
+      xaxis: {
+          lines: {
+              show: false
+          }
+      },   
+      yaxis: {
+          lines: {
+              show: false
+          }
+      },  
+    },
+    xaxis: {
+      type: "datetime",
+      // labels:{
+      //   show:false,
+      // },
+      axisTicks: {
+        show: false,
+      },
+      axisBorder: {
+        show: false,
+      },
+    },
+    dataLabels: {
+      enabled: true,
+      formatter: function (val, opts) {
+          return ((val[1]-val[0])/1000/60/60/24).toString() + " days"
+      },
+      textAnchor: 'middle',
+      offsetX: 0,
+      offsetY: 0,
+      style: {
+          fontSize: '15px',
+          fontFamily: 'Arial, Helvetica, sans-serif',
+          fontWeight: 'bold',
+          colors: ["#FAFAFA"],
+      },
+    },
+    tooltip: {
+      enabled: false,
+    },
+    series: [
+      {
+        name : "version show",
+        data: [
+          {
+            x: "IOS",
+            y: [new Date(mydata[release_version]['ios']['startdate']).getTime(),new Date(mydata[release_version]['ios']['enddate']).getTime()],
+            fillColor: "#FF4560",
+          },
+          {
+            x: "MAC",
+            y: [new Date(mydata[release_version]['mac']['startdate']).getTime(),new Date(mydata[release_version]['mac']['enddate']).getTime()],            fillColor: "#FEB019",
+          },
+          {
+            x: "SERVER",
+            y: [new Date(mydata[release_version]['server']['startdate']).getTime(),new Date(mydata[release_version]['server']['enddate']).getTime()],
+            fillColor: "#00E396"
+          },
+        ],
+      },
+    ]
+  };
+  var chart = new ApexCharts(document.querySelector("#chart"), options);
+  chart.render();
+  chart.resetSeries();
+}
